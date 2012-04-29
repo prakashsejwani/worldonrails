e749e8f5b5cf2acd09f2a38b7c5f3645e7f8cc5a
prakash <prakashsejwani@gmail.com>
2012-04-29 11:09:27 +0530

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Worldonrails::Application.config.secret_token = 'cf8d9105830b7f4b320c8463ea67f5543d66175b0a05d37e7213dd6f5e3ed23f38d52ab7aec6cb3629da08cf47de9796b342790f1c8f4b7c5855cb2729dcd27a'
